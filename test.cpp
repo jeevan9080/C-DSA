7e874e57a9f188865ad8b0e8de49f8713f551c53
jeevan9080 <r.jeevan100@gmail.com>
2022-08-09 10:30:12 +0530

    9.8.22

--- test.cpp
+++ test.cpp
@@ -1,19 +1,33 @@
 #include<iostream>
 using namespace std;
+
+void sort(int n){
+    
+    int A[20],i;
+    for(i=0;i<n;i++){
+        cin>>A[i];
+    }
+    int counter=1;
+    while(counter<n){
+        for(i=0;i<n-counter;i++){
+            if(A[i+1]<A[i]){
+                int temp=A[i];
+                A[i]=A[i+1];
+                A[i+1]=temp;
+            }
+    }
+    counter++;
+    }
+
+    for(i=0;i<n;i++){
+        cout<<A[i]<<" ";
+    }
+
+}
+
 int main(){
 
-    int a,b,c;
-    cin>>a>>b>>c;
-    if(a>b){
-        if(a>c){
-            cout<<a<<endl;
-        }
-    }
-    else if(b>c){
-        cout<<b<<endl;
-    }
-    else{
-        cout<<c<<endl;
-    }
+    int n=5;
+    sort(n);
     return 0;
 }
