206bddfe737d64e39131fcaa7256bcf581228d6a
jeevan9080 <r.jeevan100@gmail.com>
2022-08-09 09:56:39 +0530

    9.8.22

--- test.cpp
+++ test.cpp
@@ -1,39 +1,19 @@
 #include<iostream>
-#include<climits>
-#include<string>
-#include<algorithm>
 using namespace std;
-//check
 int main(){
 
-    int n,A[20],s,i;
-    cout<<"Enter:";
-    cin>>n;
-    cout<<"Elements:";
-    for(i=0;i<n;i++){
-        cin>>A[i];
-    }
-
-    bool check[20];
-    for(i=0;i<n;i++){
-        check[i]=false;
-    }
-
-    for(i=0;i<n;i++){
-        if(A[i]>=0){
-            check[A[i]]=true;
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>b){
+        if(a>c){
+            cout<<a<<endl;
         }
     }
-
-    int ans=-1;
-    for(i=0;i<n;i++){
-        if(check[i]==false){
-            ans=i;
-            break;
-        }
+    else if(b>c){
+        cout<<b<<endl;
     }
-
-    cout<<ans<<endl;
-
+    else{
+        cout<<c<<endl;
+    }
     return 0;
 }
