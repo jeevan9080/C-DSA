3c5e1051af4bf02e106d5cac242ac1120ec6a79e
Jeevan <r.jeevan100@gmail.com>
2022-08-31 22:47:35 +0530

    31.8.22

--- test.cpp
+++ test.cpp
@@ -9,46 +9,58 @@ class node{
     node(int val){
         data=val;
         next=NULL;
-    }
+    } 
 };
 
 void insertAtTail(node* &head,int val){
 
     node* n = new node(val);
-
     if(head==NULL){
         head=n;
         return;
     }
 
-    node* temp=head;
+    node* temp = head;
     while(temp->next!=NULL){
         temp=temp->next;
     }
     temp->next=n;
+
 }
 
-void deletion(node* &head,int val){
+node* reversek(node* &head,int k){
 
-    node* temp=head;
-    while(temp->next->data!=val){
-        temp=temp->next;
+    node* prevptr=NULL;
+    node* currptr=head;
+    node* nextptr;
+
+    int count=0;
+    while(currptr!=NULL && count<k){
+        nextptr=currptr->next;
+        currptr->next=prevptr;
+        prevptr=currptr;
+        currptr=nextptr;
+        count++;
     }
-    node* todelete = temp->next;
-    temp->next=temp->next->next;
 
-    delete todelete;
+    if(nextptr!=NULL){
+    head->next = reversek(nextptr,k);
+    }
+    return prevptr;
 }
+
 
 void display(node* head){
-
     node* temp=head;
     while(temp!=NULL){
         cout<<temp->data<<"->";
         temp=temp->next;
     }
+
     cout<<"NULL"<<endl;
+
 }
+
 
 int main(){
 
@@ -57,8 +69,14 @@ int main(){
     insertAtTail(head,1);
     insertAtTail(head,2);
     insertAtTail(head,3);
-    insertAtTail(head,3);
+    insertAtTail(head,4);
+    insertAtTail(head,5);
+    insertAtTail(head,6);
+
     display(head);
+    int k=2;
+    node* newhead = reversek(head,k);
+    display(newhead);
 
     return 0;
 }
