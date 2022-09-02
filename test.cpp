106015b91a503d0e16a376061277fd5741c5806b
Jeevan <r.jeevan100@gmail.com>
2022-09-02 17:04:03 +0530

    2.9.22

--- test.cpp
+++ test.cpp
@@ -1,33 +1,102 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-/*
-    stack<int> n,n1;
-    n.push(1);
-    n.push(2);
-    n.push(3);
-    n.push(4);
-    n.push(5);
-    n.push(6);
-    n1=n;
 
-    while(!n1.empty()){
-        cout<<n1.top()<<" ";
-        n1.pop();
-    }*/
+class node{
+    public:
+    int data;
+    node* next;
+    node* prev;
 
-    queue<int> n,n1;
-    n.push(1);
-    n.push(2);
-    n.push(3);
-    n.push(4);
-    n.push(5);
-    n.push(6);
-    n1=n;
+    node(int val){
+        data=val;
+        next=NULL;
+        prev=NULL;
+    }    
+};
 
-    while(!n1.empty()){
-        cout<<n1.front()<<" ";
-        n1.pop();
+void insertAtHead(node* &head,int val){
+
+    node* n = new node(val);
+    n->next=head;
+    if(head!=NULL){
+        head->prev=n;
     }
+    head=n;
+}
+
+void insertAtTail(node* &head,int val){
+
+    if(head==NULL){
+        insertAtHead(head,val);
+        return;
+    }
+
+    node* n = new node(val);
+    node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+
+    temp->next=n;
+    n->prev=temp;
+}
+
+void toDeleteHead(node* &head,int pos){
+    node* todelete = head;
+    head=head->next;
+    head->prev=NULL;
+
+    delete todelete;
+}
+
+void deletion(node* &head,int pos){
+
+    if(pos==1){
+        toDeleteHead(head,pos);
+        return;
+    }
+
+    node*temp=head;
+    int count=1;
+    while(temp!=NULL && count!=pos){
+        temp=temp->next;
+        count++;
+    }
+    temp->prev->next=temp->next;
+    if(temp->next!=NULL){
+        temp->next->prev=temp->prev;
+    }
+
+    delete temp;
+
+}
+
+void display(node* head){
+
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+int main(){
+
+    node* head=NULL;
+
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    insertAtTail(head,4);
+    insertAtTail(head,5);
+    insertAtTail(head,6);
+    display(head);
+    //insertAtHead(head,7);
+    deletion(head,3);
+    display(head);
+
+  
+
     return 0;
 }
