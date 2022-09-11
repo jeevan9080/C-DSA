ff5604f1662ac3e95dbe3b77b35f88bc9fe57438
jeevan9080 <r.jeevan100@gmail.com>
2022-09-11 22:27:23 +0530

    11.9.22

--- list/test.cpp
+++ list/test.cpp
@@ -2,21 +2,35 @@
 using namespace std;
 
 
+/*
+1 2 3
+4 5 6
+7 8 9
+*/
+
 int main(){
 
-    int n=3;
-    int A[n][n];
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin>>A[i][j];
-        }
-    }
+    int n=3,A[n][n]={{1,2,3},{4,5,6},{7,8,9}};
+    
+    int row_start=0,row_end=n-1,col_start=0,col_end=n-1,col,row;
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<A[i][j]<<" ";
+    while(row_start<=row_end && col_start<=col_end){
+        for(col=col_start;col<=col_end;col++){
+            cout<<A[row_start][col]<<" ";
         }
-        cout<<endl;
+        row_start++;
+        for(row=row_start;row<=row_end;row++){
+            cout<<A[row][col_end]<<" ";
+        }
+        col_end--;
+        for(col=col_end;col>=col_start;col--){
+            cout<<A[row_end][col]<<" ";
+        }
+        row_end--;
+        for(row=row_end;row>=row_start;row--){
+            cout<<A[row][col_start]<<" ";
+        }
+        col_start++;
     }
     return 0;
 }
