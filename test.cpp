8aaaf9bc2d761978da6c03d10c89cab0e65ba714
jeevan9080 <r.jeevan100@gmail.com>
2022-09-11 16:42:31 +0530

    11.9.22

--- test.cpp
+++ test.cpp
@@ -1,16 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void reverse(string s){
+class node{
+    public:
+    int data;
+    node* next;
 
-    for(int i=s.length()-1;i>=0;i--){
-        cout<<s[i];
+    node(int val){
+        data=val;
+        next=NULL;
     }
+    
+};
+
+void insertAtTail(node* &head,int val){
+
+    node* n = new node(val);
+
+    if(head==NULL){
+        head=n;
+        return;
+    }
+
+    node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+
+void display(node* &head){
+
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
 }
 
 int main(){
 
-    string s = "jeevan";
-    reverse(s);
+    node* head = NULL;
+
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    insertAtTail(head,4);
+    insertAtTail(head,5);
+    insertAtTail(head,6);
+
+    display(head);
+
     return 0;
 }
