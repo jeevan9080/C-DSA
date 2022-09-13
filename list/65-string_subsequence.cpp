6754b40dc58324823a76528607e10863a00abda4
Jeevan <r.jeevan100@gmail.com>
2022-09-13 16:28:57 +0530

    13.9.22

---
+++ list/65-string_subsequence.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void subsequence(string t,int i,int n,string s){
+
+    if(i==n){
+        cout<<t<<" ";
+    }
+    else{
+    subsequence(t,i+1,n,s);
+    t=t+s[i];
+    subsequence(t,i+1,n,s);
+    }
+}
+
+int main(){
+
+    string s = "abc";
+    int n=s.length();
+    subsequence("",0,n,s);
+    return 0;
+}
