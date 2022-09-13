2ae90eaabb2f1402a64c0091f551c0664bac5196
Jeevan <r.jeevan100@gmail.com>
2022-09-13 14:25:33 +0530

    13.9.22

--- list/test.cpp
+++ list/test.cpp
@@ -11,12 +11,12 @@ void printstr(string s,int start,int end){
 void longestPalStr(string s){
 
     int start=0,end=1,l,h;
-//
+
     for(int i=1;i<s.length();i++){
+
         l=i-1;
         h=i;
-
-        while(l>=0 && h<s.length() && s[l]==s[h]){
+        while(l>=0 && h<s.length() && s[h]==s[l]){
 
             if(h-l+1>end){
                 start=l;
@@ -29,7 +29,7 @@ void longestPalStr(string s){
         l=i-1;
         h=i+1;
 
-        while(l>=0 && h<s.length() && s[l]==s[h]){
+        while(l>=0 && h<s.length() && s[h]==s[l]){
 
             if(h-l+1>end){
                 start=l;
