da96cddcc53f74adefbff8c9195288adcfb34912
jeevan9080 <r.jeevan100@gmail.com>
2022-09-14 20:35:53 +0530

    14.9.22

--- list/test.cpp
+++ list/test.cpp
@@ -1,31 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int split(string s,int n){
-
-    int count0=0,count1=0,cnt=0;
-
-    for(int i=0;i<n;i++){
-        if(s[i]=='0'){
-            count0++;
-        }
-        else{
-            count1++;
-        }
-        if(count0==count1){
-            cnt++;
-        }
-    }   
-    if(count0!=count1){
-        return -1;
-    }
-    return cnt;
-}
 
 int main(){
 
-    string s = "0100110101";
-    int n=s.length();
-    cout<<split(s,n)<<endl;
+    string s = "3322251";
+    int count=1,i=0;
+    int n=s.length(); 
+
+    while(i<n){
+        if(s[i]==s[i+1]){
+            count++;
+        }
+        else{
+            cout<<count<<s[i];
+            count=1;
+        }
+        i++;     
+    }
+
     return 0;
 }
